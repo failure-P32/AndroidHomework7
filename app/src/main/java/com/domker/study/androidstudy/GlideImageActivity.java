38057f5cf7337212fadce1b669ce29a62f4afdb7
failure-P32 <493302570@qq.com>
2019-07-16 21:04:51 +0800

    code for homework 7

---
+++ app/src/main/java/com/domker/study/androidstudy/GlideImageActivity.java
@@ -0,0 +1,84 @@
+package com.domker.study.androidstudy;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlideImageActivity extends AppCompatActivity {
+    ViewPager pager = null;
+    LayoutInflater layoutInflater = null;
+    List<View> pages = new ArrayList<View>();
+
+    private final static int REQUEST_PERMISSION = 124;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        layoutInflater = getLayoutInflater();
+        pager = (ViewPager) findViewById(R.id.view_pager);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+        } else {
+            addImage(R.drawable.drawableimage);
+            addImage(R.drawable.ic_markunread);
+            addImage("/sdcard/fileimage.jpg");
+            addImage("file:///android_asset/assetsimage.jpg");
+            addImage(R.raw.rawimage);
+            addImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562328963756&di=9c0c6c839381c8314a3ce8e7db61deb2&imgtype=0&src=http%3A%2F%2Fpic13.nipic.com%2F20110316%2F5961966_124313527122_2.jpg");
+        }
+        ViewAdapter adapter = new ViewAdapter();
+        adapter.setDatas(pages);
+        pager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            addImage(R.drawable.drawableimage);
+            addImage(R.drawable.ic_markunread);
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                addImage("/sdcard/fileimage.jpg");
+            }
+            addImage("file:///android_asset/assetsimage.jpg");
+            addImage(R.raw.rawimage);
+            addImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562328963756&di=9c0c6c839381c8314a3ce8e7db61deb2&imgtype=0&src=http%3A%2F%2Fpic13.nipic.com%2F20110316%2F5961966_124313527122_2.jpg");
+            pager.getAdapter().notifyDataSetChanged();
+        }
+    }
+
+    private void addImage(int resId) {
+        ImageView imageView = (ImageView) layoutInflater.inflate(R.layout.activity_image_item, null);
+        Glide.with(this)
+                .load(resId)
+                .error(R.drawable.error)
+                .into(imageView);
+        pages.add(imageView);
+    }
+
+    private void addImage(String path) {
+        ImageView imageView = (ImageView) layoutInflater.inflate(R.layout.activity_image_item, null);
+        Glide.with(this)
+                .load(path)
+                .apply(new RequestOptions().circleCrop().diskCacheStrategy(DiskCacheStrategy.ALL))
+                .error(R.drawable.error)
+                .into(imageView);
+        pages.add(imageView);
+    }
+}
